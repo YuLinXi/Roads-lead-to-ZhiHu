d36af428343dfc21542d59a51d99556af4c20ad1
YuLinXi <15708444077@163.com>
2020-02-21 11:33:18 +0800

    :construction: 页面搭建中

---
+++ client/icons/components/live.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'live': {
+    width: 32,
+    height: 32,
+    viewBox: '0 0 1024 1024',
+    data: '<defs/><path pid="0" d="M600.747 252.587L259.413 525.653l170.667 34.134-102.4 238.933 341.333-273.067L532.48 491.52l68.267-238.933z" _fill="#ffcf00"/>'
+  }
+})
--- client/views/home/Index.vue
+++ client/views/home/Index.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="recommend">
     <div class="main-container">
-      <ul class="tabCard">
-        <li class="tabCard-item">
-          <a class="is-active">推荐</a>
-        </li>
-        <li class="tabCard-item">
-          <a>关注</a>
-        </li>
-        <li class="tabCard-item">
-          <a>热榜</a>
-        </li>
-      </ul>
-      <div class="main-content"></div>
+      <div class="recommend-ad">
+        预留banner位
+      </div>
+      <div class="main-content">
+        <ul class="tabCard">
+          <li class="tabCard-item">
+            <a class="is-active">推荐</a>
+          </li>
+          <li class="tabCard-item">
+            <a>关注</a>
+          </li>
+          <li class="tabCard-item">
+            <a>热榜</a>
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="rightSideBar">
       <div class="writeCard">
@@ -50,12 +54,160 @@
             </svg-icon>
             <div>写想法</div>
           </a>
+          <a>
+            <svg-icon
+              class="writeCard-icon"
+              name="video"
+              width="18"
+              height="20"
+              color="#8590a6"
+            >
+            </svg-icon>
+            <div>发视频</div>
+          </a>
+          <a>
+            <svg-icon
+              class="writeCard-icon"
+              name="later"
+              width="18"
+              height="20"
+              color="#8590a6"
+            >
+            </svg-icon>
+            <div>稍后答</div>
+          </a>
+          <a>
+            <svg-icon
+              class="writeCard-icon"
+              name="draft"
+              width="18"
+              height="20"
+              color="#8590a6"
+            >
+            </svg-icon>
+            <div>草稿箱</div>
+          </a>
         </div>
-        <div class="writeCard-laterOnAnswer">
+      </div>
+      <div class="writer">
+        <svg-icon
+          style="margin-left: 3px"
+          name="writer"
+          width="12"
+          height="12"
+          color="#8590a6"
+        >
+        </svg-icon>
+        <div class="writerText">创作者中心</div>
+        <div class="writerEntrance">
+          去开通
+        </div>
+        <svg-icon
+          style="margin-left: 3px"
+          name="arrowRightBorder"
+          width="18"
+          height="18"
+          color="#8590a6"
+        >
+        </svg-icon>
+      </div>
+      <div class="categoryList">
+        <div class="categoryList-item">
+          <a class="categoryList-item-link">
+            <span class="categoryList-item-icon">
+              <svg-icon
+                class="categoryList-icon"
+                name="live"
+                width="36"
+                height="36"
+                color="#ffcf00"
+              ></svg-icon>
+            </span>
+            <span class="categoryList-item-title">Live</span>
+          </a>
+        </div>
+        <div class="categoryList-item">
+          <a class="categoryList-item-link">
+            <span class="categoryList-item-icon" style="padding: 8px">
+              <svg-icon
+                class="categoryList-icon"
+                name="bookshop"
+                width="20"
+                height="20"
+                color="#43d480"
+              ></svg-icon>
+            </span>
+            <span class="categoryList-item-title">书店</span>
+          </a>
+        </div>
+        <div class="categoryList-item">
+          <a class="categoryList-item-link">
+            <span class="categoryList-item-icon" style="padding: 8px">
+              <svg-icon
+                class="categoryList-icon"
+                name="roundTable"
+                width="20"
+                height="20"
+                color="#0084ff"
+              ></svg-icon>
+            </span>
+            <span class="categoryList-item-title">圆桌</span>
+          </a>
+        </div>
+        <div class="categoryList-item">
+          <a class="categoryList-item-link">
+            <span class="categoryList-item-icon" style="padding: 7px">
+              <svg-icon
+                class="categoryList-icon"
+                name="pen"
+                width="22"
+                height="22"
+                color="#0084ff"
+              ></svg-icon>
+            </span>
+            <span class="categoryList-item-title">专栏</span>
+          </a>
+        </div>
+        <div class="categoryList-item">
+          <a class="categoryList-item-link">
+            <span class="categoryList-item-icon" style="padding: 8px">
+              <svg-icon
+                class="categoryList-icon"
+                name="pay"
+                width="20"
+                height="20"
+                color="#0f88eb"
+              ></svg-icon>
+            </span>
+            <span class="categoryList-item-title">付费咨询</span>
+          </a>
+        </div>
+        <div class="categoryList-item">
+          <a class="categoryList-item-link">
+            <span class="categoryList-item-icon" style="padding: 7px">
+              <svg-icon
+                class="categoryList-icon"
+                name="baike"
+                width="22"
+                height="22"
+                color="#0084ff"
+              ></svg-icon>
+            </span>
+            <span class="categoryList-item-title">百科</span>
+          </a>
+        </div>
+      </div>
+      <div class="advertisePosition">
+        <a class="advertisePosition-link" target="_blank">
+          <img style="width: 100%" src="https://img1.360buyimg.com/imgb/s250x250_jfs/t1/44812/11/7619/312733/5d54d323E6c88d8a2/41ddf03c04e8c795.jpg" alt="">
+        </a>
+        <div class="advertisePosition-close">
           <svg-icon
-
+            class="advertisePosition-icon"
+            name="closeBg"
+            width="20"
+            height="20"
           ></svg-icon>
-          <div>我的稍后回答</div>
         </div>
       </div>
     </div>
@@ -80,17 +232,20 @@ export default class Recommend extends Vue {}
   }
   .main-container {
     width: 694px;
-    flex-shrink: 0;
     margin-right: 10px;
-    margin-bottom: 0;
-    box-shadow: 0 1px 3px rgba(26,26,26,.1);
-    background-color: #fff;
+    .main-content {
+      flex-shrink: 0;
+      margin-bottom: 0;
+      box-shadow: 0 1px 3px rgba(26,26,26,.1);
+      background-color: #fff;
+    }
     .tabCard {
       list-style-type: none;
       height: 58px;
       display: flex;
       border-bottom: 1px solid #f0f2f7;
       &-item {
+        cursor: pointer;
         display: flex;
         align-items: center;
         font-size: 16px;
@@ -103,10 +258,18 @@ export default class Recommend extends Vue {}
         }
       }
     }
+    .recommend-ad {
+      padding: 0 16px;
+      margin-bottom: 10px;
+      height: 100px;
+      box-sizing: border-box;
+      background-color: rgba(0, 0, 0, .3);
+    }
   }
   .rightSideBar {
     flex: 1 1;
     .writeCard {
+      border-radius: 2px;
       margin-bottom: 10px;
       background: #fff;
       overflow: hidden;
@@ -116,6 +279,7 @@ export default class Recommend extends Vue {}
       &-nav {
         display: flex;
         justify-content: center;
+        flex-wrap: wrap;
         > a {
           cursor: pointer;
           display: flex;
@@ -130,12 +294,71 @@ export default class Recommend extends Vue {}
       &-icon {
         margin-bottom: 8px;
       }
-      &-laterOnAnswer {
-        margin: 0 20px;
-        height: 48px;
+    }
+    .writer {
+      border-radius: 2px;
+      height: 52px;
+      padding: 0 16px;
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      font-size: 14px;
+      line-height: 1.4;
+      margin-bottom: 10px;
+      .writerText {
+        margin-left: 12px;
         color: #8590a6;
-        display: flex;
-        align-items: center;
+        flex: 1;
+      }
+      .writerEntrance {
+        color: rgb(0, 132, 255);
+      }
+    }
+    .categoryList {
+      border-radius: 2px;
+      margin-bottom: 10px;
+      display: flex;
+      flex-wrap: wrap;
+      padding: 10px 10px 0;
+      background-color: #fff;
+      &-icon {
+        box-sizing: border-box;
+      }
+      &-item {
+        width: 33%;
+        margin-bottom: 25px;
+        &-link {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        &-icon {
+
+        }
+        &-title {
+          font-size: 15px;
+          color: #8590a6;
+        }
+      }
+    }
+    .advertisePosition {
+      position: relative;
+      overflow: hidden;
+      width: 100%;
+      transition: .35s;
+      &:hover .advertisePosition-close {
+        opacity: 1;
+      }
+      &-link {
+        width: 100%;
+      }
+      &-close {
+        position: absolute;
+        right: 10px;
+        top: 10px;
+        opacity: 0;
+        transition: .35s;
       }
     }
   }
